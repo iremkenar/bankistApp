0206944bee91b602fd67a99540d361a11ebe794a
iremkenar <irem.kenar@outlook.com>
2021-04-14 15:05:53 +0300

    Added function to close account

--- starter/script.js
+++ starter/script.js
@@ -128,7 +128,7 @@ const updateUI = function (acc) {
   calcDisplaySummary(acc);
 };
 
-//Event handler
+//Event handlers
 let currentAccount;
 
 btnLogin.addEventListener('click', function (e) {
@@ -177,4 +177,24 @@ btnTransfer.addEventListener('click', function (e) {
     //Update UI
     updateUI(currentAccount);
   }
+});
+
+btnClose.addEventListener('click', function (e) {
+  e.preventDefault();
+
+  if (
+    inputCloseUsername.value === currentAccount.username &&
+    Number(inputClosePin.value) === currentAccount.pin
+  ) {
+    const index = accounts.findIndex(
+      acc => acc.username === currentAccount.username
+    );
+    //Delete account
+    accounts.splice(index, 1);
+
+    //Hide UI
+    containerApp.style.opacity = 0;
+  }
+  //Clear input
+  inputCloseUsername.value = inputClosePin.value = '';
 });
